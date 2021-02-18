42e5b9d05bba79e7afa57f2b92fc46d6c29a9ec5
Carlos Henrique Camilo <68122899+carloshcamilo@users.noreply.github.com>
2021-02-18 12:23:32 -0300

    Add files via upload

---
+++ Vinhos/Vinhos.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import ExtraTreesClassifier
+
+arquivo = pd.read_csv('wine_dataset.csv')
+
+arquivo['style'] = arquivo['style'].replace('red', 0)
+arquivo['style'] = arquivo['style'].replace('white', 1)
+
+y = arquivo['style']
+x = arquivo.drop('style', axis = 1)
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3)
+
+modelo = ExtraTreesClassifier()
+modelo.fit(x_treino, y_treino)
+
+resultado = modelo.score(x_teste, y_teste)
+print("Acurácia: ", resultado)
